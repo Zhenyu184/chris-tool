13214e6533b4a100619d16f8795b6a0fecb53315
Zhenyu <zyy1120126@gmail.com>
2023-10-05 22:15:20 +0800

    ✨ feat: Using node index path to access all files at this path.

--- cli.js
+++
@@ -1,8 +0,0 @@
-const yargs = require('yargs');
-
-yargs
-    .command('hello', 'Display a greeting', () => {
-        console.log('Hello! This is @zhenyu184/chris-tool.');
-    })
-    .demandCommand()
-    .help().argv;
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const fs = require("fs");
-const path = require("path");
+const yargs = require('yargs');
+const fs = require('fs');
+const path = require('path');
 
 function traverseDirectory(directoryPath) {
     const items = fs.readdirSync(directoryPath);
-
     items.forEach((item) => {
         const itemPath = path.join(directoryPath, item);
         const stats = fs.statSync(itemPath);
-
         if (stats.isDirectory()) {
-            // If it's a directory, recursively traverse it
             traverseDirectory(itemPath);
         } else {
-            // If it's a file, log its path
-            console.log("File:", itemPath);
+            console.log('File:', itemPath);
         }
     });
 }
 
-module.exports = traverseDirectory;
+const argv = yargs.argv;
+const rootPath = argv._[0] ? argv._[0] : './';
+
+module.exports = traverseDirectory(rootPath.toString());
