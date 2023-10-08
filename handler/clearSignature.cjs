d0f6409c8fb80edb2504d7e31d138092f134480c
Zhenyu184 <zyy1120126@gmail.com>
2023-10-09 01:52:33 +0800

    ✨ feat: Add use regular to search for specific keywords.

--- handler/clearSignature.cjs
+++ handler/clearSignature.cjs
@@ -1,7 +1,30 @@
 const fs = require('fs');
+const config = require('../toolConfig.json');
 
 // utils
 const isJsOrTs = require('../utils/isJsOrTs.cjs');
+
+const ENV_TYPE = process.env.NODE_ENV || 'debug';
+const ENV_CONFIG = config[ENV_TYPE]['handler']['clear'];
+
+function searchString(sourceString) {
+    let item = ENV_CONFIG.exclude.join('|');
+    let regex = new RegExp(item); // RegExp(item, 'i') 忽略大小寫
+    let whetherFind = regex.test(sourceString);
+    if (whetherFind) {
+        // exclude
+        return false;
+    }
+
+    item = ENV_CONFIG.searchString.join('|');
+    regex = new RegExp(item);
+    whetherFind = regex.test(sourceString);
+    if (whetherFind) {
+        // Found
+        return true;
+    }
+    return false;
+}
 
 function clearSignature(itemPath) {
     // console.log(content);
@@ -11,11 +34,8 @@ function clearSignature(itemPath) {
         const comments = content.match(commentPattern);
         if (comments) {
             comments.forEach((element) => {
-                const searchString = 'ose';
-                const regex = new RegExp(searchString);
-                const whetherFind = regex.test(element);
-                if (whetherFind) {
-                    console.log(`找到"${searchString}":`, element);
+                if (searchString(element)) {
+                    console.log(element);
                 }
             });
         }
