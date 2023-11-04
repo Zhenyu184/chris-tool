80873ca369325054df8fdef21294b7f811495708
Zhenyu184 <zyy1120126@gmail.com>
2023-11-04 16:33:44 +0800

    💩 chore: Add text animation

--- utils/chrisProgress.cjs
+++ utils/chrisProgress.cjs
@@ -1,23 +1,41 @@
+const chalk = require('chalk');
+
 function cls() {
     process.stdout.write('\x1Bc');
 }
 
-function graph(rateNumber) {
+function graph() {
     // https://www.asciiart.eu/text-to-ascii-art
-    console.log(``);
-    console.log(` ________      ___  ___      ________     ___      ________`);
-    console.log(`|\\   ____\\    |\\  \\|\\  \\    |\\   __  \\   |\\  \\    |\\   ____\\`);
-    console.log(`\\ \\  \\___|    \\ \\  \\\\\\  \\   \\ \\  \\|\\  \\  \\ \\  \\   \\ \\  \\___|_`);
-    console.log(` \\ \\  \\        \\ \\   __  \\   \\ \\   _  _\\  \\ \\  \\   \\ \\_____  \\`);
-    console.log(`  \\ \\  \\____    \\ \\  \\ \\  \\   \\ \\  \\\\  \\   \\ \\  \\   \\|____|\\  \\`);
-    console.log(`   \\ \\_______\\   \\ \\__\\ \\__\\   \\ \\__\\\\ _\\   \\ \\__\\   |\\________\\`);
-    console.log(`    \\|_______|    \\|__|\\|__|    \\|__|\\|__|   \\|__|   \\|________|`);
-    console.log(``);
+    const ret = ` ________      ___  ___      ________     ___      ________
+|\\   ____\\    |\\  \\|\\  \\    |\\   __  \\   |\\  \\    |\\   ____\\
+\\ \\  \\___|    \\ \\  \\\\\\  \\   \\ \\  \\|\\  \\  \\ \\  \\   \\ \\  \\___|_
+ \\ \\  \\        \\ \\   __  \\   \\ \\   _  _\\  \\ \\  \\   \\ \\_____  \\
+  \\ \\  \\____    \\ \\  \\ \\  \\   \\ \\  \\\\  \\   \\ \\  \\   \\|____|\\  \\
+   \\ \\_______\\   \\ \\__\\ \\__\\   \\ \\__\\\\ _\\   \\ \\__\\   |\\________\\
+    \\|_______|    \\|__|\\|__|    \\|__|\\|__|   \\|__|   \\|________|
+`;
+    return ret;
+}
+
+function aaa(str) {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+        if (currentIndex >= str.length) {
+            clearInterval(interval);
+            return;
+        }
+
+        const char = str.charAt(currentIndex);
+        const blueChar = `\x1b[34m${char}\x1b[0m`;
+
+        process.stdout.write(blueChar);
+        currentIndex++;
+    }, 20);
 }
 
 function main() {
     console.log('chris 01');
-    graph();
+    aaa(graph());
 }
 
 main();
