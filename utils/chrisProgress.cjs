f261b0c01891f21d50982c7f2203a6ecdb083c2d
Zhenyu184 <zyy1120126@gmail.com>
2023-11-04 16:09:42 +0800

    ✨ feat: Add ascii graph

--- utils/chrisProgress.cjs
+++ utils/chrisProgress.cjs
@@ -1,14 +1,22 @@
-const jsFileExtension = ['js', 'ts', 'cjs', 'mjs', 'jsx', 'vue', 'es6'];
-
 function cls() {
     process.stdout.write('\x1Bc');
 }
 
-function graph(rateNumber) {}
+function graph(rateNumber) {
+    // https://www.asciiart.eu/text-to-ascii-art
+    console.log(``);
+    console.log(` ________      ___  ___      ________     ___      ________`);
+    console.log(`|\\   ____\\    |\\  \\|\\  \\    |\\   __  \\   |\\  \\    |\\   ____\\`);
+    console.log(`\\ \\  \\___|    \\ \\  \\\\\\  \\   \\ \\  \\|\\  \\  \\ \\  \\   \\ \\  \\___|_`);
+    console.log(` \\ \\  \\        \\ \\   __  \\   \\ \\   _  _\\  \\ \\  \\   \\ \\_____  \\`);
+    console.log(`  \\ \\  \\____    \\ \\  \\ \\  \\   \\ \\  \\\\  \\   \\ \\  \\   \\|____|\\  \\`);
+    console.log(`   \\ \\_______\\   \\ \\__\\ \\__\\   \\ \\__\\\\ _\\   \\ \\__\\   |\\________\\`);
+    console.log(`    \\|_______|    \\|__|\\|__|    \\|__|\\|__|   \\|__|   \\|________|`);
+    console.log(``);
+}
 
 function main() {
-    console.log('chris cls');
-    cls();
+    console.log('chris 01');
     graph();
 }
 
